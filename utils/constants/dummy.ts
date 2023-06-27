d809dd1e65e688a93b1fbdb568a29ffe39e95725
Dominic Lim <dominiclimkj3448@gmail.com>
2023-06-27 23:49:28 +0800

    Add additional line breaks for better look

--- utils/constants/dummy.ts
+++ utils/constants/dummy.ts
@@ -10,9 +10,9 @@ const dummy: ResultInfo[] = [
     },
     ranges: [5.2, 6.1],
     resultTab:
-      'Total amount of cholesterol in your blood includes the good (HDL cholesterol) and bad (LDL cholesterol).\nKeeping the total cholesterol within a healthy range is important. If it is too high, it could mean too much bad cholesterol. If it is too low, it could mean too little good cholesterol.',
+      'Total amount of cholesterol in your blood includes the good (HDL cholesterol) and bad (LDL cholesterol).\n\nKeeping the total cholesterol within a healthy range is important. If it is too high, it could mean too much bad cholesterol. If it is too low, it could mean too little good cholesterol.',
     tipsTab:
-      'Consume more high-fibre foods (eg. fruits, vegetables and wholegrains etc.)\nLimit food high in cholesterol (animal products like organ meat, egg yolk, prawns)\nFor more lifestyle tips, visit healthhub.com. Do  also consult a doctor if necessary for more information on managing your condition.'
+      'Consume more high-fibre foods (eg. fruits, vegetables and wholegrains etc.)\n\nLimit food high in cholesterol (animal products like organ meat, egg yolk, prawns)\n\nFor more lifestyle tips, visit healthhub.com. Do  also consult a doctor if necessary for more information on managing your condition.'
   },
   {
     title: 'Triglycerides',
@@ -22,9 +22,9 @@ const dummy: ResultInfo[] = [
     },
     ranges: [2.2, 4.5],
     resultTab:
-      'Triglycerides are a type of fat in our blood, they serve as a source of energy.\nHaving some triglycerides is necessary for body to function. But too much of it increases the risk of heart diseases and other health problems.',
+      'Triglycerides are a type of fat in our blood, they serve as a source of energy.\n\nHaving some triglycerides is necessary for body to function. But too much of it increases the risk of heart diseases and other health problems.',
     tipsTab:
-      'Great job, your triglycerides levels are healthy, keep it up! Do continue:\nMaintaining a healthy diet low in saturated fats and trans fat\nLeading an active lifestyle'
+      'Great job, your triglycerides levels are healthy, keep it up! Do continue:\n\nMaintaining a healthy diet low in saturated fats and trans fat\n\nLeading an active lifestyle'
   },
   {
     title: 'HDL Cholesterol',
@@ -34,9 +34,9 @@ const dummy: ResultInfo[] = [
     },
     ranges: [1.0, 1.6],
     resultTab:
-      'HDL (high-density lipoprotein) cholesterol is the good cholesterol.\nGenerally good to have higher levels of HDL cholesterol in your body as  it helps remove excess cholesterol from your blood vessels and protects your heart, lowering the risk of heart disease.',
+      'HDL (high-density lipoprotein) cholesterol is the good cholesterol.\n\nGenerally good to have higher levels of HDL cholesterol in your body as  it helps remove excess cholesterol from your blood vessels and protects your heart, lowering the risk of heart disease.',
     tipsTab:
-      'Consume foods high in healthy fats (e.g. avocados, almonds, cashew nuts, olive oil).\nAsk your doctor or pharmacist for help to quit smoking or call QuitLine 1800-438-2000\nFor more lifestyle tips, visit healthhub.com. Do  also consult a doctor if necessary for more information on managing your condition.'
+      'Consume foods high in healthy fats (e.g. avocados, almonds, cashew nuts, olive oil).\n\nAsk your doctor or pharmacist for help to quit smoking or call QuitLine 1800-438-2000\n\nFor more lifestyle tips, visit healthhub.com. Do  also consult a doctor if necessary for more information on managing your condition.'
   },
   {
     title: 'LDL Cholesterol',
@@ -46,9 +46,9 @@ const dummy: ResultInfo[] = [
     },
     ranges: [3.3, 4.1],
     resultTab:
-      'LDL (low-density lipoprotein) cholesterol is the bad cholesterol.\nGenerally bad to have high levels of LDL cholesterol  in your body as excess LDL cholesterol can build up in your blood vessels, narrowing it and making it hard for blood to flow. , therefore increasing the risk of heart and cardiovascular diseases.',
+      'LDL (low-density lipoprotein) cholesterol is the bad cholesterol.\n\nGenerally bad to have high levels of LDL cholesterol  in your body as excess LDL cholesterol can build up in your blood vessels, narrowing it and making it hard for blood to flow. , therefore increasing the risk of heart and cardiovascular diseases.',
     tipsTab:
-      'Limit foods high in unhealthy fats (eg. fatty meats, fried foods, and processed snacks)\nConsume more foods high in healthy fats (eg. Avocados, nuts, seeds, olive oil, tune, sardines)\n150 minutes of exercise per week (eg. brisk walking, cycling, dancing etc.)\nFor more tips, visit healthhub.com'
+      'Limit foods high in unhealthy fats (eg. fatty meats, fried foods, and processed snacks)\n\nConsume more foods high in healthy fats (eg. Avocados, nuts, seeds, olive oil, tune, sardines)\n\n150 minutes of exercise per week (eg. brisk walking, cycling, dancing etc.)\n\nFor more tips, visit healthhub.com'
   },
   {
     title: 'CHOL:HDL Ratio',
@@ -58,9 +58,9 @@ const dummy: ResultInfo[] = [
     },
     ranges: [4.5],
     resultTab:
-      'CHOL:HDL is the ratio between total cholesterol (CHOL) and high-density lipoprotein (HDL) cholesterol levels in the blood.\nA higher ratio suggests that there is a higher proportion of "bad" cholesterol in relation to the "good" cholesterol, which can indicate an increased risk of heart disease and other cardiovascular problems',
+      'CHOL:HDL is the ratio between total cholesterol (CHOL) and high-density lipoprotein (HDL) cholesterol levels in the blood.\n\nA higher ratio suggests that there is a higher proportion of "bad" cholesterol in relation to the "good" cholesterol, which can indicate an increased risk of heart disease and other cardiovascular problems',
     tipsTab:
-      'Great job, your HDL Cholesterol levels are healthy, keep it up! Do continue:\nMaintaining a healthy diet that is low in saturated fats and trans fat and high in monosaturated fats\nLeading an active lifestyle'
+      'Great job, your HDL Cholesterol levels are healthy, keep it up! Do continue:\n\nMaintaining a healthy diet that is low in saturated fats and trans fat and high in monosaturated fats\n\nLeading an active lifestyle'
   }
 ];
 
