cf0c6a6ccd2d2ef0e8dfc9effa7332f81bacf577
marcuspeh <hkmpeh@gmail.com>
2023-06-29 16:54:40 +0800

    Fix vercel crash

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -3,29 +3,29 @@
 import { Cards, Summary } from '@/components/Report';
 import TopBar from '@/components/TopBar';
 import { Flex, Text, VStack } from '@chakra-ui/react';
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 
 const navy = '#1E1E1E';
 
-export const metadata: Metadata = {
-  title: 'Report',
-  description: 'Your summarised health report',
-  openGraph: {
-    title: 'Upload health report',
-    description: 'Upload your health report for analysis',
-    url: 'https://mediclear.vercel.app',
-    siteName: 'MediClear',
-    images: [
-      {
-        url: 'https://mediclear.vercel.app',
-        width: 800,
-        height: 600
-      }
-    ],
-    locale: 'en-US',
-    type: 'website'
-  }
-};
+// export const metadata: Metadata = {
+//   title: 'Report',
+//   description: 'Your summarised health report',
+//   openGraph: {
+//     title: 'Upload health report',
+//     description: 'Upload your health report for analysis',
+//     url: 'https://mediclear.vercel.app',
+//     siteName: 'MediClear',
+//     images: [
+//       {
+//         url: 'https://mediclear.vercel.app',
+//         width: 800,
+//         height: 600
+//       }
+//     ],
+//     locale: 'en-US',
+//     type: 'website'
+//   }
+// };
 
 const Report = () => {
   return (
