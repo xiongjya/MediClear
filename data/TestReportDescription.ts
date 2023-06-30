be87f3b503a74b75b9b9a735d22570a8d514e033
marcuspeh <hkmpeh@gmail.com>
2023-06-30 23:17:39 +0800

    Update default value

---
+++ data/TestReportDescription.ts
@@ -0,0 +1,5 @@
+const TestReportDescription: {[x: string]: string} = {
+  "Lipid Panel": `Panel of blood tests used to screen for cardiovascular diseases.`
+}
+
+export default TestReportDescription;
