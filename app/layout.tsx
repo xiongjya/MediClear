0caca4de295093665605a0d2d00527265eb1c65c
marcuspeh <hkmpeh@gmail.com>
2023-07-01 12:49:30 +0800

    Prevent mobile access

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import './globals.css'
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Box, Text } from "@chakra-ui/react";
 import { Inter } from 'next/font/google'
 import TopBar from '@/components/TopBar'
+import { useState, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,30 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
+  const size = useWindowSize();
+  if (size && size.width < 900) {
+    return (
+      <html lang="en">
+        <body className={inter.className}>
+          <ChakraProvider>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100vh"
+            className="primary-bg"
+          >
+            <Box textAlign="center">
+              <Text>Mobile site is not available. Please try again on a device with a larger screen.</Text>
+            </Box>
+          </Box>
+          </ChakraProvider>
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -27,4 +52,36 @@ export default function RootLayout({
       </body>
     </html>
   )
+}
+
+// Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+     
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
